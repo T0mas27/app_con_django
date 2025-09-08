cc252f60f93ca1b8a35ee8950dc5acf88c76451a
T0mas27 <tomas2708a@gmail.com>
2025-09-08 15:08:50 -0300

    clase 5 django
    ta listo

--- bienvenida/inventario/models.py
+++ bienvenida/inventario/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 class Producto(models.Model):
     nombre = models.CharField(max_length=32)
-    Precio = models.DecimalField(max_digits=999, decimal_places=2)
+    precio = models.DecimalField(max_digits=999, decimal_places=2)
     descripcion = models.CharField(max_length=256, default="")
     stock = models.IntegerField(default=0)
--- bienvenida/inventario/views.py
+++ bienvenida/inventario/views.py
@@ -30,3 +30,18 @@ def producto_update(request, pk):
     producto = get_object_or_404(Producto, pk=pk)
     if request.method == 'POST':
         form = ProductoForm(request.POST, instance=producto)
+    if form.is_valid():
+        form.save()
+        return redirect('producto_list')
+    else:
+        form = ProductoForm(instance=producto)
+        return render(request, 'inventario/producto_form.html', {'form':
+    form})
+
+    def producto_delete(request, pk):
+        producto = get_object_or_404(Producto, pk=pk)
+    if request.method == 'POST':
+        producto.delete()
+    return redirect('producto_list')
+    return render(request,
+'inventario/producto_confirm_delete.html', {'object': producto})
